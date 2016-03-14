76406c0a88ddbacb4890d41d56d7553322b340d8
Dana Powers <dana.powers@gmail.com>
2016-03-14 00:05:38 -0700

    Add BrokerConnection.connecting()

--- kafka/conn.py
+++ kafka/conn.py
@@ -138,6 +138,10 @@ class BrokerConnection(object):
         """Return True iff socket is connected."""
         return self.state is ConnectionStates.CONNECTED
 
+    def connecting(self):
+        """Return True iff socket is in intermediate connecting state."""
+        return self.state is ConnectionStates.CONNECTING
+
     def close(self, error=None):
         """Close socket and fail all in-flight-requests.
 
